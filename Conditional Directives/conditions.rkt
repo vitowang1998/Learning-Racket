b38274b7013fb4af8ae53de5492baa90e839fc6d
Haoran Wang <vitowang1998@gmail.com>
2017-09-13 10:05:36 -0400

    Adding the syntax for conditional arguments

---
+++ Conditional Directives/conditions.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname conditions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; This is the syntax for conditional arguments in Racket
+(cond
+  ((= 1 2) "1 = 2")
+  ((= 2 2) "2 = 2")
+  ((= 5 5) "5 = 5"))
