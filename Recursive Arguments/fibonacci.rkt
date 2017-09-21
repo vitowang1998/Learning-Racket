e504bb588884ee2774dfefffc0c8eb6518fc594a
Haoran Wang <vitowang1998@gmail.com>
2017-09-20 21:55:14 -0400

    Adding Fibonacci Sequence Generator

---
+++ Recursive Arguments/fibonacci.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname fibonacci) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; A typical recursive function
+
+(define (fib n)
+  (cond
+    ((= n 0) 0)
+    ((= n 1) 1)
+    (else (+ (fib (- n 1)) (fib (- n 2))))))
+
+
