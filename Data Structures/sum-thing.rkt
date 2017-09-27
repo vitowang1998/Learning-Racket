91cdc008309c5232b40cd6d9fac9ba83c7476652
Haoran Wang <vitowang1998@gmail.com>
2017-09-27 16:59:47 -0400

    Added a file about calculating the sum in a user-defined data structure

---
+++ Data Structures/sum-thing.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname sum-thing) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; This is user-defined data structure
+(define-struct thing (a b))
+
+
+
+; Regardless of the conponents in thing structure, we will be able to calculate the sum of all components in it.
+(define (sum-thing n)
+  (cond
+    [(number? n) n]
+    [(and (number? (thing-a n)) (number? (thing-b n))) (+ (thing-a n) (thing-b n))]
+    [true (+ (sum-thing (thing-a n)) (sum-thing (thing-b n)))]))
+
+
+
