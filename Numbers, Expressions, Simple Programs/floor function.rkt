d7c97937d354653b80483cb50e3120f35be72ede
Haoran Wang <vitowang1998@gmail.com>
2017-09-27 16:20:06 -0400

    Added a file about floor function

---
+++ Numbers, Expressions, Simple Programs/floor function.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |floor function|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; floor function
+
+(floor 3.1)
+(floor 4.0)
+(floor 0)
+(floor -1.5)  ; Note that the returned value of this operation is -2
