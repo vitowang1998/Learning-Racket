6ed6b69c0e4ea5c6f3fd22c4d12fad044166f5c6
Haoran Wang <vitowang1998@gmail.com>
2017-10-07 17:24:07 -0400

    Added a file about user-defined errors in Racket

---
+++ Basics/error.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname error) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; error
+
+
+(define (sampleFunction inputNum)
+  (cond
+    [(number? inputNum) inputNum]
+    [else (error 'sampleFunction "Number expected")]))
+
+(sampleFunction 1)
+
+
+
+;; Error
+(sampleFunction "HelloWorld")
