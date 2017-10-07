cba3cbce354aed52765b5b0e74d4a3c8aa1eecbe
Haoran Wang <vitowang1998@gmail.com>
2017-10-07 17:03:07 -0400

    created a new folder about lists
    List is an important data stucture in Racket.

--- Untitled.rkt
+++
@@ -1,9 +0,0 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname Untitled) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(define-struct node (left right))
-
-; a----------------------------------------------------------------------------
-(define (tree-similar a b)
-  [(and (empty? a) (empty? b)) true]
-  [(and (
