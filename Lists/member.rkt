58b3f945c89e260a55a0b6b9a55ec15a5177e383
Haoran Wang <vitowang1998@gmail.com>
2017-10-12 23:39:28 -0400

    Added a file about checking membership of an element

---
+++ Lists/member.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname member) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Member of list
+
+;; A function to determine if e is in lst
+;; O(n)
+(define (member e lst)
+  (cond
+    [(empty? lst) false]
+    [(= e (car lst)) true]
+    [else (member e (cdr lst))]))
