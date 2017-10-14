4f9debbfd218ecf5833ee0b14aa736bee809d8d8
Haoran Wang <vitowang1998@gmail.com>
2017-10-14 11:08:37 -0400

    Modified the function and added an error case

--- Lists/abs-max.rkt
+++ Lists/abs-max.rkt
@@ -3,12 +3,18 @@
 #reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname abs-max) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; yields the item with maximum absolute value
 
+
 (define (abs-max l)
   (cond
-    [(= 1 (length l)) (abs (car l))]
+    [(empty? l) (error 'abs-max "A non-empty list expected")]
+    [(= 1 (length l)) (car l)]
     [else (if (< (abs (car l)) (abs (abs-max (cdr l))))
               (abs-max (cdr l))
               (car l))]))
+
+
+(abs-max (list))
+
 
 
 (abs-max (list -99 -1 1 2 3 4 5 -6))
