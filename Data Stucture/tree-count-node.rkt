f76d5752d643eeec45b9c1fc4fa0041e1787031f
Haoran Wang <vitowang1998@gmail.com>
2017-10-15 21:58:04 -0400

    Added a file about counting the number of nodes in a tree

---
+++ Data Stucture/tree-count-node.rkt
@@ -0,0 +1,11 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname count-node) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; Count the number of node
+(define (count-node t)
+  (cond
+    [(empty? t) 0]
+    [else (+ 1 (count-node (node-left t)) (count-node (node-right t)))]))
+
+
+
