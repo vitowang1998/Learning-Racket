f1d0e765faa8e07420c2bf5b36a992330379d5c8
Haoran Wang <vitowang1998@gmail.com>
2017-10-21 21:06:23 -0400

    Modified the design recipe in if-and-condition

--- Conditional Directives/if-and-condition.rkt
+++ Conditional Directives/if-and-condition.rkt
@@ -1,16 +1,16 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname if&condition) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; There are actually two ways of doint it.
-; Obviously, we can use the built-in min and max function to eliminate the largest and smallest term
+;; There are actually two ways of doint it.
+;; Obviously, we can use the built-in min and max function to eliminate the largest and smallest term
 
 
 
-; Condition is easier to read
-; if has less code.
+;; Condition is easier to read
+;; if has less code.
 
 
-; Condition:
+;; Condition:
 
 #|
 (define (median a b c)
@@ -24,7 +24,8 @@
 
 |#
 
-; If:
+;; If:
 
+;; O(n) = 1
 (define (median a b c)
   (if (<= a b) (if (<= b c) b (if (<= a c) c a)) (if (>= b c) b (if (<= a c) a c))))
