3d0d3f2b110c47b589b23c0862be2b94ce168238
Haoran Wang <vitowang1998@gmail.com>
2017-10-21 21:05:13 -0400

    Added the time complexty of cond directives

--- Conditional Directives/conditions.rkt
+++ Conditional Directives/conditions.rkt
@@ -2,6 +2,9 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname conditions) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ; This is the syntax for conditional arguments in Racket
+
+
+;; O(n) = 1
 (cond
   ((= 1 2) "1 = 2")
   ((= 2 2) "2 = 2")
