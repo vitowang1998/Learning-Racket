9c91c892f7bc3fa41882eba5881c95f3d2ab8f70
Haoran Wang <vitowang1998@gmail.com>
2017-10-21 21:04:42 -0400

    Added the time complexty (O) of collinear function

--- Conditional Directives/collinear?.rkt
+++ Conditional Directives/collinear?.rkt
@@ -3,5 +3,7 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname collinear) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 ;; This is a function used to check if the 3 points given are collinear
 ;; This is also a demonstration to the built-in function "if"
-(define (collinear x1 y1 x2 y2 x3 y3)
+
+;; O(n) = 1
+(define (collinear? x1 y1 x2 y2 x3 y3)
   (if (and (= (* (- y2 y1) (- x3 x1)) (* (- x2 x1) (- y3 y1))) (= (* (- y3 y2) (- x3 x1)) (* (- y3 y1) (- x3 x2)))) true false))
