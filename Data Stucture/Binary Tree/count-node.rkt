ae8b238ec593f69d10ada9e2e880aa17ebd6a16d
Haoran Wang <vitowang1998@gmail.com>
2017-10-23 00:24:53 -0400

    Added the time complexity of a function

--- Data Stucture/Binary Tree/count-node.rkt
+++ Data Stucture/Binary Tree/count-node.rkt
@@ -1,8 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname tree-count-node) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname count-node) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct node (left right))
+
+
 ;; Count the number of nodes in a tree
-;;
+;; O(n) = n^2
 (define (count-node t)
   (cond
     [(empty? t) 0]
