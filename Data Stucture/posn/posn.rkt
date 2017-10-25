7b33ca5ece860973e45af59e6841b464583607a3
Haoran Wang <vitowang1998@gmail.com>
2017-10-25 16:29:44 -0400

    Modified design recipe and time complexity

--- Data Stucture/posn/posn.rkt
+++ Data Stucture/posn/posn.rkt
@@ -1,21 +1,28 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname posn) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; Default data structure
-; posn for Position
+;; Default data structure
+;; posn for Position
+
+;; Running time: O(1)
 (make-posn 1 2)
 
-; Check if a variable is posn
+;; Check if a variable is posn
+;; Running time: O(1)
 (posn? (make-posn 1 2))
 
-; Get the 1st variable
+;; Get the 1st variable
+;; Running time: O(1)
 (posn-x (make-posn 12 13))
 
-; Get the 2nd variable
+;; Get the 2nd variable
+;; Running time: O(1)
 (posn-y (make-posn 14 15))
 
-; Since we are calculating the distance to origin, we assume that
-; (and (number? (posn-x r-posn)) (number? (posn-y r-posn)))
+;; Since we are calculating the distance to origin, we assume that
+;; (and (number? (posn-x r-posn)) (number? (posn-y r-posn)))
+
+;; Running time: O(1)
 (define (distance-to-0 r-posn)
   (sqrt (+ (sqr (posn-x r-posn)) (sqr (posn-y r-posn)))))
 
