3294288982377d1180f8f66ffd2970246b5e3e69
Haoran Wang <vitowang1998@gmail.com>
2017-10-27 15:51:21 -0400

    slight modification

--- data stucture/list/get-size:list.rkt
+++ data stucture/list/get-size:list.rkt
@@ -1,10 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname get-size-of-list) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname size:list) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 
 ;; A function to get the size of a list
-;; O(n) 
-(define (size/list lst)
+;; O(n)
+;; list -> number
+(define (get-size/list lst)
   (cond
     [(empty? lst) 0]
     [else (add1 (getsize-list (cdr lst)))]))
