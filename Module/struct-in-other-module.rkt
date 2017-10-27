b832829efb9098fad70f07154b05b0e617893100
Haoran Wang <vitowang1998@gmail.com>
2017-10-27 13:28:19 -0400

    Added a specific rule in Racket

---
+++ Module/struct-in-other-module.rkt
@@ -0,0 +1,4 @@
+#lang racket
+;; data-structure created from another module is not accessable from this module
+;; Even though we defined a data-structure with the same name, the built-in functions
+;; wouldn't work for the struct defined in that module
