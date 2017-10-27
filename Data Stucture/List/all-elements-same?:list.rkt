8037ea3ef92209c715a2a6e1ebd562ce5c8bacbf
Haoran Wang <vitowang1998@gmail.com>
2017-10-26 22:08:42 -0400

    Added a function to determine if all the elements in a list are same

---
+++ Data Stucture/List/all-elements-same?:list.rkt
@@ -0,0 +1,15 @@
+#lang racket
+;; A function to check if all the elements in a list are same
+
+;; O(n)
+(define (all-elements-same?/list lst)
+  (cond
+    [(empty? lst) true] ; you can modify this by demands
+    [(= 2 (length lst)) (= (car lst) (car (cdr lst)))] ; when the number of elements of list is 2, compare the elements
+    [(= (car lst) (car (cdr lst))) (all-elements-same?/list (cdr lst))] ; if same, compare next
+    [else false])) ; if the first two elem. in list are not same, false
+
+(define la (list 1 2 3 4 5 6 7 8))
+(define lb (list 1 3 4 5 2 4 4 4))
+(define lc empty)
+(define ld (list 1 1 1 1 1))
