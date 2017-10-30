7200c1597888afbbc45ead95fb4283ff2be007c9
Haoran Wang <vitowang1998@gmail.com>
2017-10-29 22:24:03 -0400

    Added a function to check if a number is an element of a posn.

--- data stucture/posn/element-of:posn.rkt
+++ data stucture/posn/element-of:posn.rkt
@@ -1,13 +1,18 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname element-of) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-;; element-of
 (define a (make-posn 213 12345))
 
-(define (element-of b n)
+
+
+
+;; check if b is an element of a posn
+;; O(n)
+;; number, posn -> bool
+(define (element-of/posn b n)
   (cond
     [(number? b) (= b n)]
-    [else (or (element-of (posn-x b) n) (element-of (posn-y b) n))]))
+    [else (or (element-of/posn (posn-x b) n) (element-of/posn (posn-y b) n))]))
 
 
 (element-of a 123)
