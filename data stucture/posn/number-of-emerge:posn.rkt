418eef8eea3c8ad9d26c9a004565d9f93c6c3acd
Haoran Wang <vitowang1998@gmail.com>
2017-11-03 15:13:38 -0400

    Added a function to check the number of emergence of an element in a list

--- data stucture/posn/number-of-emerge:posn.rkt
+++ data stucture/posn/number-of-emerge:posn.rkt
@@ -2,11 +2,12 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-advanced-reader.ss" "lang")((modname number-of-emergence) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
 ;; Number of emergence of an element in a posn
-
-(define (number-of pair n)
+;; O(n)
+;; posn + int -> int
+(define (number-of-emerge/posn pair n)
   (cond
     [(number? pair) (if (= pair n) 1 0)]
-    [else (+ (number-of (posn-x pair)) (number-of (posn-y pair)))]))
+    [else (+ (number-of-emerge/posn (posn-x pair)) (number-of-emerge/posn (posn-y pair)))]))
 
 
 ; Everything that adds up can be solved with the algorithms above
