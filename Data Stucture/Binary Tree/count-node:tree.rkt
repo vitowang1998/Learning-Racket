d323d9ed3be1011f173bcd526626ff54bb9a3656
Haoran Wang <vitowang1998@gmail.com>
2017-11-08 07:47:13 -0500

    Added design recipe for a function.

--- Data Stucture/Binary Tree/count-node:tree.rkt
+++ Data Stucture/Binary Tree/count-node:tree.rkt
@@ -1,11 +1,12 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname count-node) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname count-node:tree) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (define-struct node (left right))
 
 
 ;; Count the number of nodes in a tree
 ;; Running time: O(n^2)
+;; tree -> number
 (define (count-node/tree t)
   (cond
     [(empty? t) 0]
