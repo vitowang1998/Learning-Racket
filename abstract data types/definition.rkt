9f2c21c1b65386845124d1d4762cc762db3adaec
Haoran Wang <vitowang1998@gmail.com>
2017-11-09 13:47:42 -0500

    Added an understanding to ADT.

--- abstract data types/definition.rkt
+++ abstract data types/definition.rkt
@@ -3,4 +3,6 @@
 ;; Many sources share the same view on the definition of abstract data type.
 ;; They consider an abstract data type is defined by its behaviours.
 ;; In essence, an abstract data type is the essence of the data it represents.
-;; And the vaious ways of implementing this abstract data type is the coresponding data strctures for it.
+;; And the vaious ways of implementing this abstract data type are the coresponding data strctures for it.
+
+;; In some sense, we are simulating the machine-level interactions of programs.
