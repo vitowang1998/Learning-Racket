39822267026e301156b2d32631f70b5cbc4dfda1
Haoran Wang <vitowang1998@gmail.com>
2017-11-14 17:59:38 -0500

    Added a function to insert a number into a bst.

---
+++ data stucture/binary search tree/insert:bst.rkt
@@ -0,0 +1,21 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname Untitled) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct node (left right key))
+
+
+;; insert a number into a binary search tree
+;; O(n) in worst case
+;; O(log n) usually
+;; insert/bst: bst + int -> bst
+(define (insert/bst t n)
+  (cond
+    [(empty? t) (make-node empty empty n)]
+    [(= n (node-key t)) t]
+    [(< n (node-key t)) (make-node (insert/bst (node-left t) n) (node-right t) (node-key t))]
+    [else (make-node (node-left t) (insert/bst (node-right t) n) (node-key t))]))
+
+
+(define test (insert/bst empty 0))
+(define test2 (insert/bst test 2))
+    
