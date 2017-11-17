554cb841e2d0c95d8b951722f49492811966de33
Haoran Wang <vitowang1998@gmail.com>
2017-11-16 19:18:04 -0500

    Added the leaf?/bst function to the bst.rkt file.

--- data stucture/binary search tree/bst.rkt
+++ data stucture/binary search tree/bst.rkt
@@ -1,5 +1,5 @@
 #lang racket
-(provide insert/bst)
+(provide insert/bst leaf?/bst)
 
 (define-struct node (left right key))
 
@@ -14,7 +14,19 @@
     [(< n (node-key t)) (make-node (insert/bst (node-left t) n) (node-right t) (node-key t))]
     [else (make-node (node-left t) (insert/bst (node-right t) n) (node-key t))]))
 
+
+;; check if a node is a leaf of a bst
+;; O(1)
+;; leaf?/bst: node -> boolean
+;; special case: note that an empty tree is also a leaf here
+(define (leaf?/bst bst)
+  (and (empty? (node-left bst))
+       (empty? (node-right bst))))
+
+
+
 ;; (insert/bst): insert a number into a binary search tree
+;; (leaf?/bst): 
 ;; ------
 ;; (delete)
 ;; (member?/bst)
