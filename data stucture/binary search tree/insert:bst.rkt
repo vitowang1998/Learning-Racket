f54945f38e6870b39805d48974e753ca1246fe83
Haoran Wang <vitowang1998@gmail.com>
2017-11-17 12:53:05 -0500

    Added a more efficient version of insert/bst.

--- data stucture/binary search tree/insert:bst.rkt
+++ data stucture/binary search tree/insert:bst.rkt
@@ -1,14 +1,22 @@
-;; The first three lines of this file were inserted by DrRacket. They record metadata
-;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname insert:bst) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-(define-struct node (left right key))
-
+#lang racket
+(define-struct node (left right key)#:transparent)
 
 
 ;; insert a number into a binary search tree
 ;; O(n) in worst case
 ;; O(log n) usually
 ;; insert/bst: bst + int -> bst
+(define (insert2/bst t n)
+  (if (empty? t) (make-node empty empty n)
+      ; else
+      (let ([current (node-key t)])
+        (cond
+          [(= n current) t]
+          [(< n current) (make-node (insert2/bst (node-left t) n) (node-right t) current)]
+          [else (make-node (node-left t) (insert2/bst (node-right t) n) current)]))))
+
+
+;; Another way of implementing it
 (define (insert/bst t n)
   (cond
     [(empty? t) (make-node empty empty n)]
@@ -19,4 +27,7 @@
 
 ;; (define test (insert/bst empty 0))
 ;; (define test2 (insert/bst test 2))
-    
+
+
+
+
