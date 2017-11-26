37f4a38ff57b90f4769d9bbd08dbee8f91766760
Haoran Wang <vitowang1998@gmail.com>
2017-11-26 16:05:52 -0500

    Added explaination to the idea of y-combinator.

--- Y combinator/Y-combinator.rkt
+++ Y combinator/Y-combinator.rkt
@@ -3,6 +3,9 @@
 ;; Basically, Y combinator implements recursion in a special case where recursion is not permitted.
 ;; In other words, the essence of Y combinator converts the long recursive function to a huge line.
 
+
+
+
 ;; sample 1
 ;; recursive function
 (define fac
@@ -18,5 +21,42 @@
           (* x (fact (sub1 x)))))))
 
 
+;; use Y-combinator
+(define Y
+  (λ (f)
+    ((λ(x)
+       (f (x x)))
+     (λ(x)
+       (f (x x))))))
+
+(define fac-yc
+  (Y
+   (λ (fact-yc)
+     (λ (n)
+       (if (zero? n) 1
+           (* n (fact-yc (sub1 n))))))))
+
+
 ;; sample 2
 ;; recursive function
+
+
+;; self application in Lambda calculus
+;; loop = (λx.x.x) (λx.x.x) ; apply this function to itself
+
+; another example
+(define (rec f)
+  (f (rec f)))
+
+; if we run it
+;; (rec f) = (f (f (f (f (rec f)))))
+
+
+;;
+(define loop
+  (rec loop))
+;;
+
+
+;; Y combinator
+
