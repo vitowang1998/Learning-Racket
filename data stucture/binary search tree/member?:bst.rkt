5c1eb21edf6908186a3a7ed3929d08fb6fde884f
Haoran Wang <vitowang1998@gmail.com>
2017-12-03 16:12:39 -0500

    Added "member?/bst.rkt".

---
+++ data stucture/binary search tree/member?:bst.rkt
@@ -0,0 +1,12 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname member?:bst) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct node (left right key))
+
+(define (member?/bst bst n)
+  (if (empty? bst) false
+      (let ([value (bst-node-val bst)])
+        (cond
+          [(= n value) true]
+          [(< n value) (member?/bst (bst-node-left bst) n)]
+          [(> n value) (member?/bst (bst-node-right bst) n)]))))
