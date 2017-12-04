47bcbf07bb497c458ff88ed209201488672cb205
Haoran Wang <vitowang1998@gmail.com>
2017-12-03 22:55:14 -0500

    Minor modification.

--- Data Stucture/Binary Tree/get-height:tree.rkt
+++ Data Stucture/Binary Tree/get-height:tree.rkt
@@ -1,12 +1,13 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname tree-height) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname get-height:tree) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
 (define-struct node (left right))
 
 
 
 ;; Get the height of a tree
 ;; O(n) = n
+;; get-height/tree: tree -> number
 (define (get-height/tree t)
   (cond
     [(empty? t) 0]
