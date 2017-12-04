e0f0b16a1dd93d304b950285a63c2a60bb5adb2f
Haoran Wang <vitowang1998@gmail.com>
2017-12-03 23:30:53 -0500

    Added a function to test if a tree/bst is balanced.

---
+++ data stucture/binary search tree/balanced?:tree.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define-struct node (left right key))
+
+;; checks if a tree is balanced
+;; O(n) where n is the size of tree
+;; 
+(define (balanced?/tree t)
+  ;; Count the number of nodes in a tree
+  ;; Running time: O(n)
+  ;; tree -> number
+  (define (count-node/tree t)
+    (cond
+      [(empty? t) 0]
+      [else (+ 1 (count-node/tree (node-left t)) (count-node/tree (node-right t)))]))
+
+  (cond
+    [(empty? t) true]
+    [else (and (balanced?/tree (node-left t))
+               (balanced?/tree (node-right t))
+               (<= -1 (- (count-node/tree (node-left t)) (count-node/tree (node-right t))) 1))]))
