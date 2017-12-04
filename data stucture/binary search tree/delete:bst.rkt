8245126b2fabaeba1df7b84c59677429433c5960
Haoran Wang <vitowang1998@gmail.com>
2017-12-04 13:52:49 -0500

    Corrected the function of deleting a node from a bst.

--- data stucture/binary search tree/delete:bst.rkt
+++ data stucture/binary search tree/delete:bst.rkt
@@ -1,23 +1,33 @@
 #lang racket
+
 (define-struct node (left right key)#:transparent)
 
-(define (insert/bst t n)
+;; delete a value from a bst
+;; O(log2 n), worst case O(n/2)
+;; delete/bst: bst + number -> bst
+(define (delete/bst t n)
+  
+  ;; check if a node is a leaf of a bst
+  ;; O(1)
+  ;; leaf?/bst: node -> boolean
+  (define (leaf?/bst bst)
+    (and (empty? (node-left bst))
+         (empty? (node-right bst))))
+
+  ;; removes the root of a bst, only used when we have located the value we want to delete
+  ;; O(log2 n)
+  ;; remove-root/bst: bst -> bst
+  (define (remove-root/bst t)
+    (cond
+      [(empty? t) t]
+      [(leaf?/bst t) empty]
+      [(empty? (node-left t)) (node-right t)]
+      [(empty? (node-right t)) (node-left t)]
+      [else (make-node (node-left t) (remove-root/bst (node-right t)) (node-key t))]))
+  
   (cond
-    [(empty? t) (make-node empty empty n)]
-    [(= n (node-key t)) t]
-    [(< n (node-key t)) (make-node (insert/bst (node-left t) n) (node-right t) (node-key t))]
-    [else (make-node (node-left t) (insert/bst (node-right t) n) (node-key t))]))
+    [(empty? t) empty]
+    [(= n (node-key t)) (remove-root/bst t)]
+    [(> n (node-key t)) (make-node (node-left t) (delete/bst (node-right t) n) (node-key t))]
+    [else (make-node (delete/bst (node-left t) n) (node-right t) (node-key t))]))
 
-
-(define (delete/bst bst n)
-  (cond
-    [(empty? bst) empty]
-    [(= n (node-key bst)) empty]
-    [(> n (node-key bst)) (make-node (node-left bst) (delete/bst (node-right bst) n) (node-key bst))]
-    [else (make-node (delete/bst (node-left bst) n) (node-right bst) (node-key bst))]))
-
-
-(define test (insert/bst empty 2))
-(define test2 (insert/bst (insert/bst test 12) 14))
-(node-key test)
-(delete/bst  test 2)
