4d03345a32b42457781bf69cf7acbe5db4c88150
Haoran Wang <vitowang1998@gmail.com>
2017-12-03 23:12:25 -0500

    Added another function in "bst.rkt".

--- data stucture/binary search tree/bst.rkt
+++ data stucture/binary search tree/bst.rkt
@@ -2,7 +2,8 @@
 (provide insert/bst
          leaf?/bst
          num-member?/bst
-         get-min/bst)
+         get-min/bst
+         full?/bst)
 
 (define-struct node (left right key))
 
@@ -56,11 +57,42 @@
     [(empty? (node-left t)) (node-key t)]
     [else (get-min/bst (node-left t))]))
 
+;; --------------------------------------------------------------------
+
+;; checks if a tree is a full tree
+;; O(n) where n is the number of nodes
+;; full?/tree: tree -> boolean
+(define (full?/bst t)
+  
+  ;; Count the number of nodes in a tree
+  ;; Running time: O(n)
+  ;; tree -> number
+  (define (count-node/tree t)
+    (cond
+      [(empty? t) 0]
+      [else (+ 1 (count-node/tree (node-left t)) (count-node/tree (node-right t)))]))
+  
+  ;; Get the height of a tree
+  ;; O(n) = n
+  ;; get-height/tree: tree -> number
+  (define (get-height/tree t)
+    (cond
+      [(empty? t) 0]
+      [else (+ 1 (max (get-height/tree (node-left t)) (get-height/tree (node-right t))))]))
+  
+  (cond
+    [(empty? t) true]
+    [(= (sub1 (expt 2 (get-height/tree t))) (count-node/tree t)) true]
+    [else false]))
+
+
+
 
 ;; (insert/bst): insert a number into a binary search tree
 ;; (leaf?/bst): check if a node is a leaf of a bst
 ;; (num-member?/bst): check if a number is a member of a bst.
 ;; (get-min/bst): get the minimum of a bst.
+;; (full?/bst): is a bst full?
 ;; ------
 ;; (delete)
 ;; (bst?)
